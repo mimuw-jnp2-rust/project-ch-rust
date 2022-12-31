35b71d51d278dbe30582bd85fbd6e24920fa8e38
Antoni Koszowski <58612067+akoszowski@users.noreply.github.com>
2022-12-31 15:13:56 +0100

    :spider_web:  P2P Networking (#7)
    * Add p2p networking
    
    * Add generic params

---
+++ src/p2p.rs
@@ -0,0 +1,118 @@
+use super::{App, Block};
+use libp2p::{
+    floodsub::{Floodsub, FloodsubEvent, Topic},
+    identity,
+    mdns::{Mdns, MdnsEvent},
+    swarm::{NetworkBehaviourEventProcess, Swarm},
+    NetworkBehaviour, PeerId,
+};
+use log::{error, info};
+use once_cell::sync::Lazy;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use std::convert::identity;
+use tokio::sync::mpsc;
+
+pub static KEYS: Lazy = Lazy::new(identity::Keypair::generete_ed25519);
+pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChainResponse {
+    pub blocks: Vec<Block>,
+    pub receiver: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct LocalChainRequest {
+    pub from_peer_id: String,
+}
+
+pub enum EventType {
+    LocalChainResponse(ChainResponse),
+    Input(String),
+    Init,
+}
+
+#[derive(NetworkBehaviour)]
+pub struct AppBehaviour {
+    pub floodsub: Floodsub,
+    pub mdns: Mdns,
+    #[behaviour(ignore)]
+    pub response_sender: mpsc::UnboundedSender<ChainResponse>,
+    #[behaviour(ignore)]
+    pub init_sender: mpsc::UnboundedSender<bool>,
+    #[behaviour(ignore)]
+    pub app: App,
+}
+
+impl AppBehaviour {
+    pub async fn new(
+        app: App,
+        response_sender: mpsc::UnboundedSender<ChainResponse>,
+        init_sender: mpsc::UnboundedSender<bool>,
+    ) -> Self {
+        let mut behaviour = Self {
+            app,
+            floodsub: Floodsub::new(*PEER_ID),
+            mdns: Mdns::new(Default::default())
+                .await
+                .expect("Can created mdns."),
+            response_sender,
+            init_sender,
+        };
+        behaviour.floodsub.subscribe(CHAIN_TOPIC.clone());
+        behaviour.floodsub.subscribe(BLOCK_TOPIC.clone());
+
+        behaviour
+    }
+}
+
+impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: FloodsubEvent) {
+        if let FloodsubEvent::Message(msg) = event {
+            if let Ok(res) = serde_json::from_slice::<ChainResponse>(&msg.data) {
+                if res.receiver = PEER_ID.to_string() {
+                    info!("Response from {}:", msg.source);
+                    res.blocks.iter().for_each(|r| info!("{:?}", r));
+
+                    self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), res.blocks);
+                }
+            } else if Ok(res) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+                info!("Sending local chain to {}", msg.source.to_string());
+                let peer_id = res.from_peer_id;
+                if PEER_ID.to_string() == peer_id {
+                    if let Err(e) = self.response_sender.send(ChainResponse {
+                        blocks: self.app.blocks.clone(),
+                        receiver: msg.source.to_string(),
+                    }) {
+                        error!("Error sending response via channel, {}", e);
+                    }
+                }
+            } else if let Ok(block) = serde_json::from_slice::<Block>(&msg.data) {
+                info!("Received new block from {}", msg.source.to_string());
+                self.app.try_add_block(block);
+            }
+        }
+    }
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(discovered_list) => {
+                for (peer, _addr) in discovered_list {
+                    self.floodsub.add_node_to_partial_view(peer);
+                }
+            }
+            MdnsEvent::Expired(expired_list) => {
+                for (peer, _addr) in expired_list {
+                    if ! self.mdns.has_node(&peer) {
+                        self.floodsub.remove_node_from_partial_view(&peer);
+                    }
+                }
+            }
+        }
+    }
+}
