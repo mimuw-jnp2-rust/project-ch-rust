75410ffbdabc8d5df9ac9f555ea76f746b235507
akoszowski <a.koszowski@students.mimuw.edu.pl>
2022-12-31 17:38:59 +0100

    Add p2p network printers

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,4 +1,3 @@
-use super::{App, Block};
 use libp2p::{
     floodsub::{Floodsub, FloodsubEvent, Topic},
     identity,
@@ -8,15 +7,15 @@ use libp2p::{
 };
 use log::{error, info};
 use once_cell::sync::Lazy;
+use project_ch_rust::{App, Block};
 use serde::{Deserialize, Serialize};
 use std::collections::HashSet;
-use std::convert::identity;
 use tokio::sync::mpsc;
 
-pub static KEYS: Lazy = Lazy::new(identity::Keypair::generete_ed25519);
-pub static PEER_ID: Lazy = Lazy::new(|| PeerId::from(KEYS.public()));
-pub static CHAIN_TOPIC: Lazy = Lazy::new(|| Topic::new("chains"));
-pub static BLOCK_TOPIC: Lazy = Lazy::new(|| Topic::new("blocks"));
+pub static KEYS: Lazy<identity::Keypair> = Lazy::new(identity::Keypair::generate_ed25519);
+pub static PEER_ID: Lazy<PeerId> = Lazy::new(|| PeerId::from(KEYS.public()));
+pub static CHAIN_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("chains"));
+pub static BLOCK_TOPIC: Lazy<Topic> = Lazy::new(|| Topic::new("blocks"));
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ChainResponse {
@@ -73,13 +72,13 @@ impl NetworkBehaviourEventProcess<FloodsubEvent> for AppBehaviour {
     fn inject_event(&mut self, event: FloodsubEvent) {
         if let FloodsubEvent::Message(msg) = event {
             if let Ok(res) = serde_json::from_slice::<ChainResponse>(&msg.data) {
-                if res.receiver = PEER_ID.to_string() {
+                if res.receiver == PEER_ID.to_string() {
                     info!("Response from {}:", msg.source);
                     res.blocks.iter().for_each(|r| info!("{:?}", r));
 
                     self.app.blocks = self.app.choose_chain(self.app.blocks.clone(), res.blocks);
                 }
-            } else if Ok(res) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
+            } else if let Ok(res) = serde_json::from_slice::<LocalChainRequest>(&msg.data) {
                 info!("Sending local chain to {}", msg.source.to_string());
                 let peer_id = res.from_peer_id;
                 if PEER_ID.to_string() == peer_id {
@@ -108,11 +107,55 @@ impl NetworkBehaviourEventProcess<MdnsEvent> for AppBehaviour {
             }
             MdnsEvent::Expired(expired_list) => {
                 for (peer, _addr) in expired_list {
-                    if ! self.mdns.has_node(&peer) {
+                    if !self.mdns.has_node(&peer) {
                         self.floodsub.remove_node_from_partial_view(&peer);
                     }
                 }
             }
         }
+    }
+}
+
+pub fn get_list_peers(swarm: &Swarm<AppBehaviour>) -> Vec<String> {
+    info!("Discovered Peers:");
+    let nodes = swarm.behaviour().mdns.discovered_nodes();
+    let mut unique_peers = HashSet::new();
+    for peer in nodes {
+        unique_peers.insert(peer);
+    }
+    unique_peers.iter().map(|p| p.to_string()).collect()
+}
+
+pub fn handle_print_peers(swarm: &Swarm<AppBehaviour>) {
+    let peers = get_list_peers(swarm);
+    peers.iter().for_each(|p| info!("{}", p));
+}
+
+pub fn handle_print_chain(swarm: &Swarm<AppBehaviour>) {
+    info!("Local Blockchain:");
+    let pretty_json =
+        serde_json::to_string_pretty(&swarm.behaviour().app.blocks).expect("Can jsonify blocks.");
+    info!("{}", pretty_json);
+}
+
+pub fn handle_create_block(cmd: &str, swarm: &mut Swarm<AppBehaviour>) {
+    if let Some(data) = cmd.strip_prefix("create b") {
+        let behaviour = swarm.behaviour_mut();
+        let latest_block = behaviour
+            .app
+            .blocks
+            .last()
+            .expect("There is at least one block");
+        let block = Block::new(
+            latest_block.id + 1,
+            latest_block.hash.clone(),
+            data.to_owned(),
+        );
+        let json = serde_json::to_string(&block).expect("Can jsoinfy requrst.");
+        behaviour.app.blocks.push(block);
+        info!("Broadcasting new block");
+        behaviour
+            .floodsub
+            .publish(BLOCK_TOPIC.clone(), json.as_bytes());
     }
 }
